938bdd04b9ae40d5fb7aafd7cbd0f0fc925d1c1c
dani4feed <dan.arbatov@ukr.net>
2025-01-15 20:04:19 +0200

    modpackInstaller 0.6.6 --Icon added, version listing added

--- view.py
+++ view.py
@@ -81,7 +81,8 @@ class Window:
         self.d_path.set(self.default_d_path)
         self.minecraft_path = self.default_d_path
 
-        self.root.title("Modpack Installer")
+        self.root.title("Modpack Installer 0.6.5.1")
+        self.root.iconbitmap("src/image.ico")
         self.root.configure(background=self.back)
         self.root.minsize(self.root_with, self.root_height)
         self.root.maxsize(self.root_with, self.root_height)
